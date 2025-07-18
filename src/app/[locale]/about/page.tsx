0b0cf4ecd7eb4dd127fee575e94fc394fa9fa91f
Lili Sazonova <lilissazonova@gmail.com>
2025-07-18 17:01:02 +0300

    fix about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -12,7 +12,7 @@ const About: React.FC = () => {
         <h1 className="font-lora text-[40px] text-center uppercase mb-[34px] md:text-[64px] md:mb-[48px]">
           {t('title')}
         </h1>
-        <div className="flex flex-col gap-4 md:flex-rowmd:flex-wrap md:justify-between md:gap-[29px] xl:gap-[46px]">
+        <div className="flex flex-col gap-4 md:flex-row md:flex-wrap md:justify-between md:gap-[29px] xl:gap-[46px]">
           <Image
             src="/Images/about_foto.jpg"
             alt="О нас"
@@ -21,7 +21,7 @@ const About: React.FC = () => {
             className="block mx-auto w-[320px] h-[482px] md:w-[344px] md:h-[518px] xl:w-[474px] xl:h-[712px]"
           />
           <div className="flex flex-col gap-6">
-            <p className="font-inter text-[18px] leading-[21.78px] text-left  max-w-[355px] xl:max-w-[708px]">
+            <p className="font-inter text-[18px] leading-[21.78px] text-сenter max-w-[355px] xl:max-w-[708px]">
               {t('description')}
             </p>
             <p className="font-inter text-[18px] leading-[21.78px] text-left  max-w-[355px] xl:max-w-[708px]">
