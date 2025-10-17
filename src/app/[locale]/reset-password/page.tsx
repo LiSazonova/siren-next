e172e972f385bc503d915f3c41ff7f3ec1f27ddb
Lili Sazonova <lilissazonova@gmail.com>
2025-10-17 12:27:40 +0300

    fix reset page

--- src/app/[locale]/reset-password/page.tsx
+++ src/app/[locale]/reset-password/page.tsx
@@ -238,3 +238,12 @@
 //     </section>
 //   );
 // }
+'use client';
+
+export default function ResetPasswordPage() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-black text-white">
+      <p>Reset password page temporarily disabled.</p>
+    </section>
+  );
+}
