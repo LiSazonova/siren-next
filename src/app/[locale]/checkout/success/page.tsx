c2000709df155ff1b96ea23e432a135a19115455
Lili Sazonova <lilissazonova@gmail.com>
2026-03-22 15:48:26 +0200

    fix checkout success page

--- src/app/[locale]/checkout/success/page.tsx
+++ src/app/[locale]/checkout/success/page.tsx
@@ -3,29 +3,30 @@ import { redirect } from 'next/navigation';
 import SuccessClient from './SuccessClient';
 
 type Props = {
-  params: {
+  params: Promise<{
     locale: string;
-  };
+  }>;
   searchParams: Promise<{
     order?: string;
   }>;
 };
 
 export default async function SuccessPage({ params, searchParams }: Props) {
+  const { locale } = await params;
   const { order } = await searchParams;
 
   if (!order) {
-    redirect(`/${params.locale}/checkout/error`);
+    redirect(`/${locale}/checkout/error`);
   }
 
   const orderData = await getOrderById(order);
 
   if (!orderData) {
-    redirect(`/${params.locale}/checkout/error`);
+    redirect(`/${locale}/checkout/error`);
   }
 
   if (orderData.paymentStatus === 'failed') {
-    redirect(`/${params.locale}/checkout/error`);
+    redirect(`/${locale}/checkout/error`);
   }
 
   return (
